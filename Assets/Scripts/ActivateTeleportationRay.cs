e7bdbf2bf39b172885ebcee37f1f726561499632
Chouquette <lydie.sts@gmail.com>
2024-03-19 23:21:55 +0100

    Tunneling pour réduire la motion sickeness
    Pour réduire la motion sickness :
    - tunneling pour la téléportation
    - réduction du champs de vision pour les déplacements
    
    + désactivation du rayon de téléportation si on a un objet en main
    + désactivation du force grab sur le rayon de téléportation (qui nous permettait d'attraper un objet à distance)

--- Assets/Scripts/ActivateTeleportationRay.cs
+++ Assets/Scripts/ActivateTeleportationRay.cs
@@ -7,30 +7,17 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class ActivateTeleportationRay : MonoBehaviour
 {
-    /*[SerializeField] private GameObject leftTeleportation;
-    [SerializeField] private GameObject rightTeleportation;
-
-    [SerializeField] private InputActionProperty leftActivate;
-    [SerializeField] private InputActionProperty rightActivate;
-
-    private void Update()
-    {
-        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
-    }
-
-    */
     [SerializeField] private GameObject leftTeleportation;
     [SerializeField] private GameObject rightTeleportation;
 
     [SerializeField] private InputActionProperty leftActivate;
     [SerializeField] private InputActionProperty rightActivate;
-    
-    //[SerializeField] private InputActionProperty leftCancel;
-    //[SerializeField] private InputActionProperty rightCancel;
 
     [SerializeField] private XRRayInteractor leftRay;
     [SerializeField] private XRRayInteractor rightRay;
+        
+    [SerializeField] private XRDirectInteractor leftDirectRay;
+    [SerializeField] private XRDirectInteractor rightDirectRay;
 
     private bool isLeftRayHovering_ = false;
     private bool isRightRayHovering_ = false;
@@ -40,10 +27,10 @@ public class ActivateTeleportationRay : MonoBehaviour
     {
         isLeftRayHovering_ = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber,
             out bool leftValue);
-        leftTeleportation.SetActive(!isLeftRayHovering_ /*&& leftCancel.action.ReadValue<float>() == 0*/ && leftActivate.action.ReadValue<float>() > 0.1f);
+        leftTeleportation.SetActive(!isLeftRayHovering_ && leftActivate.action.ReadValue<float>() > 0.1f && leftDirectRay.interactablesSelected.Count == 0);
         
         isRightRayHovering_ = rightRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber,
             out bool rightValue);
-        rightTeleportation.SetActive(!isRightRayHovering_ /*&& rightCancel.action.ReadValue<float>() == 0*/ && rightActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(!isRightRayHovering_ && rightActivate.action.ReadValue<float>() > 0.1f && rightDirectRay.interactablesSelected.Count == 0);
     }
 }
